389f5f7aa4298bc15afe0a0b3b79ddc5d9a00869
Jon Reyes <jonrey3@gmail.com>
2021-01-16 01:49:25 -0500

    Add Main Fix

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+let principal = document.getElementById('principal');
+
+let rate = document.getElementById('rate');
+let rateslide = document.getElementById('rateslide');
+rate.innerText = rateslide.value+"%";
+rateslide.addEventListener('input',updateRate,false);
+function updateRate(){
+	let rate = document.getElementById('rate');
+	let rateslide = document.getElementById('rateslide');
+	rate.innerText = rateslide.value+"%";
 }
+
+let result = document.getElementById('result');
+result.style.display = "none";
+
+function compute()
+{	
+	let result = document.getElementById('result');
+	let principal = document.getElementById('principal');
+	let amount = Number.parseInt(principal.value);
+	let rateslide = document.getElementById('rateslide');
+	let time = Number.parseInt(document.getElementById('years').value);
+    let endYear = new Date().getFullYear() + time;
+    
+    result.style.display = "block";
+    let resultvalue = amount*(1+rateslide.value/100)**(time)
+    resultvalue -= amount;
+    resultvalue = resultvalue.toFixed(2) 
+    let resulttext = `If you deposit <mark>${amount}</mark>,</br>
+    at an interest rate of <mark>${rateslide.value}%</mark>.</br>
+    You will receieve an amount of <mark>${resultvalue}</mark></br>
+    in the year <mark>${endYear}</mark>.`;
+    
+    result.innerHTML = resulttext;
+}
+
+
         
